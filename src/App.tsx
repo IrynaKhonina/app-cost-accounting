3866483ae685ca06837a6b1e66605fea4e112220
IrynaKhonina <irinahonina@yahoo.com>
2024-11-09 14:39:01 +0300

    09,11,2024

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { ConstItem } from "./components/Constitem";
+
 
 function App() {
   return (
     <div>
       <h1>поехали</h1>
+      <ConstItem/>
     </div>
   );
 }
---
+++ src/components/Constitem.tsx
@@ -0,0 +1,15 @@
+ 
+ 
+ export const ConstItem =()=> {
+    return (
+      <div>
+        <div>март 2021 12</div>
+        <div>
+        <h2>Холодильник</h2>
+        <div>$999.99</div>
+        </div>
+    </div>
+    );
+  }
+
+  
