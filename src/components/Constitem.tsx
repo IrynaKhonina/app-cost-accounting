e43f1aebd1cdbe6a47c2b4ec97df4a8de3459e24
IrynaKhonina <irinahonina@yahoo.com>
2024-12-22 20:10:28 +0300

    22

--- src/components/Constitem.tsx
+++ src/components/Constitem.tsx
@@ -13,13 +13,21 @@ export const ConstItem = (props: CostItemProps) => {
         description,
         amount,
     } = props
+
+    const month =date.toLocaleDateString("ru-RU", {month: "long"})
+    const year=date.getFullYear()
+    const day = date.getDate().toString().padStart(2, "0");
     return (
       <div className="cost-item">
-        <div>{date.toISOString()}</div>
-        <div className="cost-item__description">
-        <h2>{description}</h2>
-        <div className="cost-item__price">${amount}</div>
-        </div>
+          <div>
+              <div>{month}</div>
+              <div>{year}</div>
+              <div>{day}</div>
+          </div>
+          <div className="cost-item__description">
+              <h2>{description}</h2>
+              <div className="cost-item__price">${amount}</div>
+          </div>
     </div>
     );
   }
