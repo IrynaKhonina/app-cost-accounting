faebfbca0df827ebaa8d4490a5fa65dfe533e479
IrynaKhonina <irinahonina@yahoo.com>
2025-01-02 16:56:56 +0300

    32

--- src/components/Costs/ConstItem.tsx
+++ src/components/Costs/ConstItem.tsx
@@ -10,14 +10,17 @@ export type CostItemProps = {
 
 export const CostItem = (props: CostItemProps) => {
     const { date, description, amount } = props;
+    const changeDiscriptionHandler = () =>{
 
+    }
     return (
         <Card className="cost-item">
             <CostDate date={date} />
-            <div className="cost-item__description">
+            <div className="cost-item__description">.
                 <h2>{description}</h2>
                 <div className="cost-item__price">${amount}</div>
             </div>
+            <button onClick={changeDiscriptionHandler}>Изменить описание</button>
         </Card>
     );
 };
